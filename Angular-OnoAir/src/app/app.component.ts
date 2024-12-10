4ad295008106f0bd41ef03838562bd837118ddf7
NoyDror <noydror44@gmail.com>
2024-12-10 05:49:04 +0200

    Creating the "header", "footer" and "homepage" components

--- Angular-OnoAir/src/app/app.component.ts
+++ Angular-OnoAir/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-onoair';
+  title = 'OnoAir';
 }
