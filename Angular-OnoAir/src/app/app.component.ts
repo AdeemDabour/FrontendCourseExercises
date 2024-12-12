757e00829e44a212ac25ae2c9735364295811c00
NoyDror <noydror44@gmail.com>
2024-12-13 01:06:42 +0200

    Installing Angular Material

--- Angular-OnoAir/src/app/app.component.ts
+++ Angular-OnoAir/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, HomepageComponent],
+  imports: [HeaderComponent, FooterComponent, HomepageComponent, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
