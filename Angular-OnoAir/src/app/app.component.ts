08360b73100a7303155e85b51a5acc8590639d8c
NoyDror <noydror44@gmail.com>
2024-12-13 03:35:04 +0200

    Making links so that when you click on the buttons in the main menu you are taken to the appropriate page & clicking on the title in the header takes you to the main page

--- Angular-OnoAir/src/app/app.component.ts
+++ Angular-OnoAir/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomepageComponent } from './homepage/homepage.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, HomepageComponent, MatIconModule, MatButtonModule],
+  imports: [HeaderComponent, FooterComponent, MatIconModule, MatButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Angular-OnoAir/src/app/app.routes.ts
+++ Angular-OnoAir/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ManageFlightsComponent } from './manage-flights/manage-flights.component';
+import { ManageDestinationsComponent } from './manage-destinations/manage-destinations.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'manage-destinations', component: ManageDestinationsComponent },
+  { path: 'manage-flights', component: ManageFlightsComponent },
+  { path: 'search-flight', component: FlightSearchComponent },
+  { path: 'my-bookings', component: MyBookingsComponent },
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: '**', redirectTo: '/homepage' } 
+];
