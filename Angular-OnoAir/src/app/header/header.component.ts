c90df2898836ed98e767ab134ebbb7a8ee9c3564
NoyDror <noydror44@gmail.com>
2024-12-13 02:51:00 +0200

    Creating the components "my-bookings", flight-search", "manage-destinations"" and "manage-flights" & designing the title and creating + designing the main menu

--- Angular-OnoAir/src/app/header/header.component.ts
+++ Angular-OnoAir/src/app/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  
+
 }
