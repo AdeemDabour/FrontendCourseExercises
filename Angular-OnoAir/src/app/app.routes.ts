b6266e05814d62c8ee97beda84473a8acdbcba2b
NoyDror <noydror44@gmail.com>
2024-12-13 10:01:34 +0200

    Correcting the component name to 'home-page' and adding the line in the app.config.ts file

--- Angular-OnoAir/src/app/app.routes.ts
+++ Angular-OnoAir/src/app/app.routes.ts
@@ -1,16 +1,15 @@
 import { Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ManageFlightsComponent } from './manage-flights/manage-flights.component';
 import { ManageDestinationsComponent } from './manage-destinations/manage-destinations.component';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { MyBookingsComponent } from './my-bookings/my-bookings.component';
 
 export const routes: Routes = [
-  { path: 'homepage', component: HomepageComponent },
+  { path: 'home-page', component: HomePageComponent },
   { path: 'manage-destinations', component: ManageDestinationsComponent },
   { path: 'manage-flights', component: ManageFlightsComponent },
   { path: 'search-flight', component: FlightSearchComponent },
   { path: 'my-bookings', component: MyBookingsComponent },
-  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-  { path: '**', redirectTo: '/homepage' } 
+  { path: '', component: HomePageComponent },
 ];
--- Angular-OnoAir/src/app/manage-flights/manage-flights.component.ts
+++ Angular-OnoAir/src/app/manage-flights/manage-flights.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-manage-flights',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './manage-flights.component.html',
-  styleUrl: './manage-flights.component.css'
+  styleUrls: ['./manage-flights.component.css']
 })
 export class ManageFlightsComponent {
+  
+  flights = [
+    { number: 'LY001', destination: 'New York' },
+    { number: 'LY002', destination: 'London' },
+    { number: 'LY003', destination: 'Paris' },
+  ];
+  
+  displayedColumns: string[] = ['flightNumber', 'destination', 'actions'];
 
+  onAddFlight() {
+    console.log('Add Flight button clicked');
+  }
+
+  onEditFlight(flight: any) {
+    console.log('Edit Flight:', flight);
+  }
 }
