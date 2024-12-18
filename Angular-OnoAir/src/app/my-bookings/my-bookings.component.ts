b26dbfcb332e794f205057e8f3640533b5f4836a
AdeemDabour <adem.druze619@gmail.com>
2024-12-18 22:45:55 +0200

    changed "My-bookings" page to show as conatainers(for now only) instead of a sorted list ...

--- Angular-OnoAir/src/app/my-bookings/my-bookings.component.ts
+++ Angular-OnoAir/src/app/my-bookings/my-bookings.component.ts
@@ -3,12 +3,12 @@ import { BookingService, Booking } from '../bookings.service';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatSortModule, MatSort, Sort } from '@angular/material/sort';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-my-bookings',
-  imports: [MatSortModule, MatTableModule, DatePipe],
+  imports: [CommonModule,MatSortModule, MatTableModule, DatePipe],
   templateUrl: './my-bookings.component.html',
   styleUrl: './my-bookings.component.css'
 })
