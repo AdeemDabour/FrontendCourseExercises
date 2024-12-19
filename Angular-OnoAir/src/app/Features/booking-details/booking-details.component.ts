f2ec2cac0a30883df2aedfad302e3a2b2f19ad50
NoyDror <noydror44@gmail.com>
2024-12-19 20:49:35 +0100

    A "back" button has been added to all flight, booking and destination details pages

--- Angular-OnoAir/src/app/Features/booking-details/booking-details.component.ts
+++ Angular-OnoAir/src/app/Features/booking-details/booking-details.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-booking-details',
-  imports: [MatCardModule, MatDivider ,CommonModule],
+  imports: [MatCardModule, MatDivider ,CommonModule, MatButtonModule, RouterLink],
   templateUrl: './booking-details.component.html',
   styleUrl: './booking-details.component.css'
 })
