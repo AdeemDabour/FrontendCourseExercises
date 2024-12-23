1cc4f885d0dd22d31577d3771311da7c1f33ea11
NoyDror <noydror44@gmail.com>
2024-12-23 07:17:13 +0100

    Display a message if there are no flights in the future/this week in the flight management table and for last-minute flights

--- Angular-OnoAir/src/app/Features/home/pages/last-minute-flights/last-minute-flights.component.ts
+++ Angular-OnoAir/src/app/Features/home/pages/last-minute-flights/last-minute-flights.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { RouterLink } from '@angular/router';
 import { Flight } from '../../../flights/model/flight';
 import { FlightsService } from '../../../flights/service/flights.service';
+import { FlightCardComponent } from '../flight-card/flight-card.component';
 
 @Component({
   selector: 'app-last-minute-flights',
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatButtonModule, MatListModule, MatGridListModule, RouterLink],
+  imports: [CommonModule, MatGridListModule, FlightCardComponent],
   templateUrl: './last-minute-flights.component.html',
   styleUrl: './last-minute-flights.component.css'
 })
@@ -19,7 +16,8 @@ export class LastMinuteFlightsComponent implements OnInit {
 
   lastMinuteFlights: Flight[] = [];
 
-  constructor(private flightsService: FlightsService) { }
+  constructor(private flightsService: FlightsService) {}
+
   ngOnInit(): void {
     this.lastMinuteFlights = this.flightsService.getFlightsThisWeek();
   }
