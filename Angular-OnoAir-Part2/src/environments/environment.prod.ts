56eac3de942221a2156b9a828698d904185c277f
AdeemDabour <adem.druze619@gmail.com>
2025-01-07 02:55:11 +0200

    few changes in packages so firebase could run , added "environments" folder to hold firebase Configurations.

---
+++ Angular-OnoAir-Part2/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyA0SkUmt8srXGcT1MaW5wX2glxBqUuS0Vc",
+      authDomain: "onoair-3a09d.firebaseapp.com",
+      projectId: "onoair-3a09d",
+      storageBucket: "onoair-3a09d.appspot.com",
+      messagingSenderId: "242311591205",
+      appId: "1:242311591205:web:c7cef800917f01040dd6cc",
+      measurementId: "G-MCYP7N1S59"
+    }
+  };
+  
