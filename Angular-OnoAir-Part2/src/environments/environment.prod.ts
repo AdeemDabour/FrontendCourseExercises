8efd50c683f05af673768549c38f826caaffddfb
AdeemDabour <adem.druze619@gmail.com>
2025-01-16 08:37:17 +0200

    deleted earlier commits by noy since i got lost and dont know whats the problems are, updated the envirnomt with new api & firebase info , since the last one was public.

--- Angular-OnoAir-Part2/src/environments/environment.prod.ts
+++ Angular-OnoAir-Part2/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
     firebase: {
-      apiKey: "AIzaSyA0SkUmt8srXGcT1MaW5wX2glxBqUuS0Vc",
+      apiKey: "AIzaSyBh8F1KMuCVlJouYFLPZJclI2QVyV1c2jM",
       authDomain: "onoair-3a09d.firebaseapp.com",
       projectId: "onoair-3a09d",
-      storageBucket: "onoair-3a09d.appspot.com",
+      storageBucket: "onoair-3a09d.firebasestorage.app",
       messagingSenderId: "242311591205",
-      appId: "1:242311591205:web:c7cef800917f01040dd6cc",
-      measurementId: "G-MCYP7N1S59"
+      appId: "1:242311591205:web:c316ee6e1f626b250dd6cc",
+      measurementId: "G-3WJG27X55P"
     }
   };
   
