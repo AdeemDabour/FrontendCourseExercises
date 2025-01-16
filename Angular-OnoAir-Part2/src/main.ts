557a594c38a830273af2975086cb759fdf33f033
AdeemDabour <adem.druze619@gmail.com>
2025-01-16 09:00:43 +0200

    fixed menu button & book now page working perfectly , the app was missing animations function , and added it in main.ts.

--- Angular-OnoAir-Part2/src/main.ts
+++ Angular-OnoAir-Part2/src/main.ts
@@ -5,11 +5,13 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideFirebaseApp(() => initializeApp(environment.firebase)), //intialize firebase
     provideFirestore(() => getFirestore()), //initialize firestore
     provideRouter(routes),
+    provideAnimations(),
   ],
 }).catch((err) => console.error(err));
